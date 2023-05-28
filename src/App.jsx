35878d87de9d660d1bbf7b3049d77179acb73e0c
Alberto-Knox <volkankarakaya2020@gmail.com>
2023-05-28 19:55:33 +0300

    add paragraph rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
+import data from "./data";
 
 const App = () => {
   const [count, setCount] = useState(1);
   const [text,setText] = useState([])
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    setText(data.slice(0,amount))
+
+  }
+
   return (
-    <section className="section-center">
+    <section className="section-center" onSubmit={handleSubmit}>
       <h4>tired of boring lorem ipsum?</h4>
       <form className="lorem-form">
         <label htmlFor="amount">paragraphs</label>
@@ -21,6 +29,11 @@ const App = () => {
         />
         <button className="btn" type="submit">generate</button>
       </form>
+      <article className="lorem-text">
+        {text.map((item,index) => {
+          return <p key={index}>{item}</p>
+        })}
+      </article>
     </section>
   );
 };
